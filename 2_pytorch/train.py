d9b1e9b09eda09711c8af31d5f667659625d4549
pramodith <pballapuram3@gatech.edu.com>
2018-09-23 17:07:29 -0400

    DL

--- 2_pytorch/train.py
+++ 2_pytorch/train.py
@@ -94,7 +94,7 @@ elif args.model == 'twolayernn':
 elif args.model == 'convnet':
     model = models.convnet.CNN(im_size, args.hidden_dim, args.kernel_size,
                                n_classes)
-    model=torch.load("DL1\\2_pytorch\convnetvgg_16.pt")
+    model=torch.load("convnetvgg_16.pt")
 elif args.model == 'mymodel':
     model = models.mymodel.MyModel(im_size, args.hidden_dim,
                                args.kernel_size, n_classes)
